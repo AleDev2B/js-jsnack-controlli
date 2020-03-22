8e47fc43f9ccacca335f74d6ec08ddc35b8006a5
Alessandro <alecritelli@gmail.com>
2020-03-22 22:38:08 +0100

    inserted comments on ex.2

--- ex.2/main.js
+++ ex.2/main.js
@@ -6,19 +6,20 @@ var Userword1 = prompt("Ciao Frenzo, tenta la fortuna ed inserisci la parola for
 // utente2 inserisce la parola al prompt
 var Userword2 = prompt("Ciao Pasquale, tenta la fortuna ed inserisci la parola fortunata")
 
-
+// sistema verifica che la parola scritta dal 1° utente sia più piccola e la stampa a video
 if (Userword1 < Userword2) {
   document.getElementById('LengthWordWin').innerHTML = "Ciao Frenzo hai vinto!!, hai inserito la parola:" + Userword1;
-
+// il sistema stampa la parola inserita dal 2° utente
   document.getElementById('WordLose').innerHTML =  Userword2 ;
 }
 
+// sistema verifica che la parola scritta dal 2° utente sia più piccola e la stampa a video
 else if (Userword2 < Userword1) {
   document.getElementById('LengthWordWin').innerHTML = "Ciao PASQUALE hai vinto!!, hai inserito la parola:" + Userword2;
-
+// il sistema stampa la parola inserita dal 1° utente
   document.getElementById('WordLose').innerHTML =  Userword1 ;
 }
-
+// condizione finale di parole uguale tra i due
 else {
 document.getElementById('LengthWordWin').innerHTML = "Avete inserito parole della stessa lunghezza"
 }
